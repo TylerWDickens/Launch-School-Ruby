fb3d552bc3a89a95d71a7228a07b18cd48331c6b
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-04-12 21:24:48 -0700

    Methods Excercises

---
+++ Methods/Exercise3.rb
@@ -0,0 +1,15 @@
+
+
+def multiply(x,y)
+  result = x * y
+end
+
+puts "Enter first number to multiply"
+x = gets.to_i
+
+puts "Enter second number to multiply"
+y = gets.to_i
+
+product = multiply(x,y)
+
+puts "Your result is #{product}"
